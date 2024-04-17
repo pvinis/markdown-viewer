67bf1c3c680874fad8aa8f95987bf12df2d837d0
Pavlos Vinieratos <pvinis@gmail.com>
2024-04-17 21:25:45 +0300

    try

--- src/MDTWView.tsx
+++ src/MDTWView.tsx
@@ -1,25 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 
-const host = "https://f000.backblazeb2.com/file/dropshare-public-pavlos";
+// const host = "https://f000.backblazeb2.com/file/dropshare-public-pavlos";
 
-const fetchFile = async (file: string) => {
-  const res = await fetch(`${host}${file}`);
-  return res.text();
-};
+// const fetchFile = async (file: string) => {
+//   const res = await fetch(`${host}${file}`);
+//   return res.text();
+// };
 
 export function MDTWView({ file }: { file: string }) {
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: [file],
-    queryFn: () => fetchFile(file),
-  });
+  // const { data, isLoading, isError, error } = useQuery({
+  //   queryKey: [file],
+  //   queryFn: () => fetchFile(file),
+  // });
 
-  if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>Error: {String(error)}</p>;
+  // if (isLoading) return <p>Loading...</p>;
+  // if (isError) return <p>Error: {String(error)}</p>;
 
   return (
     <article className="prose lg:prose-xl">
       <p>{file}</p>
-      {data}
+      {/* {data} */}
     </article>
   );
 }
