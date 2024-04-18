abf362975fa8173670bc0363de4aec63e8313814
Pavlos Vinieratos <pvinis@gmail.com>
2024-04-18 14:23:40 +0300

    test

--- src/MDTWView.tsx
+++ src/MDTWView.tsx
@@ -1,27 +1,37 @@
-// import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-// const host = "https://f000.backblazeb2.com/file/dropshare-public-pavlos";
+const host = "https://f000.backblazeb2.com/file/dropshare-public-pavlos";
 
-// const fetchFile = async (file: string) => {
-//   const res = await fetch(`${host}${file}`);
-//   return res.text();
-// };
+const fetchFile = async (file: string) => {
+  const res = await fetch(`${host}${file}`);
+  return res.text();
+};
 
 export function MDTWView({ file }: { file: string }) {
-  // const { data, isLoading, isError, error } = useQuery({
-  //   queryKey: [file],
-  //   queryFn: () => fetchFile(file),
-  // });
+  const {
+    data: markdown,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: [file],
+    queryFn: () => fetchFile(file),
+  });
 
-  // if (isLoading) return <p>Loading...</p>;
-  // if (isError) return <p>Error: {String(error)}</p>;
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {String(error)}</p>;
 
   return (
-    <article className="prose lg:prose-xl">
-      <p>{file}</p>
-      {/* {data} */}
-    </article>
+    <div className="px-6 pt-32 pb-16 max-w-prose prose lg:prose-xl mx-auto prose-blockquote:overline">
+      <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+    </div>
   );
 }
 
-/// mattersomething on top
+/// mattersomething on top for title and favicon?
+
+//dark mode
+
+// frontmatter
