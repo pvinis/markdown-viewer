85072e10a6421e8fe3017e9a567abef95ac9a4ee
Pavlos Vinieratos <pvinis@gmail.com>
2024-04-18 15:19:06 +0300

    aha

--- src/404.tsx
+++
@@ -1,8 +0,0 @@
-export function FourOhFour() {
-  return (
-    <article className="prose lg:prose-xl">
-      <h1>404</h1>
-      <p>Page not found</p>
-    </article>
-  );
-}
--- src/pages/[file].tsx
+++ src/pages/[file].tsx
@@ -1,28 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { useParams } from "../router";
 
 const host = "https://f000.backblazeb2.com/file/dropshare-public-pavlos";
 
 const fetchFile = async (file: string) => {
-  console.log("fetching", `${host}${file}`);
-  const res = await fetch(`${host}${file}`);
+  const res = await fetch(`${host}/${file}`);
   return res.text();
 };
 
-export function MDTWView({ file }: { file: string }) {
+export default function MDTWView() {
+  const { file } = useParams("/:file");
+
   const {
     data: markdown,
     isLoading,
     isError,
     error,
+    ...rest
   } = useQuery({
     queryKey: [file],
     queryFn: () => fetchFile(file),
+    retry: false,
   });
 
-  if (isLoading) return <p>Loading...</p>;
+  console.log({ markdown, isLoading, isError, error, ...rest });
   if (isError) return <p>Error: {String(error)}</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <div className="px-6 pt-32 pb-16 max-w-prose prose lg:prose-xl mx-auto prose-blockquote:overline">
