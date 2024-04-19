c529cdf390388eb5e668cfa58454275c3916322a
Pavlos Vinieratos <pvinis@gmail.com>
2024-04-19 16:33:23 +0300

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ export default function HomePage() {
   return (
     <>
       <h1>Markdown Tailwind Viewer</h1>
-      <p>by pavlos</p>
-      <Link href="/intro.md">Intro</Link>
+      <p>by Pavlos Vinieratos</p>
+      <p>
+        Check out the <Link href="/intro.md">intro</Link>
+      </p>
     </>
   );
 }
 
 // spinner for loading? or some simple dots?
-
-/// commit .env
 
 ///try witouhgvercel.json file
