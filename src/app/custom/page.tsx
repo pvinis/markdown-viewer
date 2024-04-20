9b0c6e60a5510549210902502d9358ed7457e253
Pavlos Vinieratos <pvinis@gmail.com>
2024-04-20 15:03:06 +0300

    building

--- src/app/custom/page.tsx
+++ src/app/custom/page.tsx
@@ -1,36 +1,27 @@
 "use client"
 
-import { useQuery } from "@tanstack/react-query"
+import { MDRenderer } from "@/MDRenderer"
+import Link from "next/link"
 import { useSearchParams } from "next/navigation"
-import Markdown from "react-markdown"
-import remarkGfm from "remark-gfm"
-
-const fetchFile = async (url: string) => {
-	const res = await fetch(url)
-	return res.text()
-}
 
 export default function CustomPage() {
 	const searchParams = useSearchParams()
-	const u = searchParams.get("u")
+	const url = searchParams.get("u")
+	/// make it so url is restricted, so i don t fire random requests
 
-	const {
-		data: markdown,
-		isLoading,
-		isError,
-		error,
-	} = useQuery({
-		queryKey: [u],
-		enabled: !!u,
-		queryFn: () => fetchFile(u!),
-		retry: false,
-	})
+	if (!url)
+		return (
+			<>
+				<h2>Wrong url</h2>
+				<p>
+					Try going to the <Link href="/intro.md">intro</Link> instead 🙂
+				</p>
+			</>
+		)
 
-	if (!u) return <p>Missing URL parameter</p>
-	if (isError) return <p>Error: {String(error)}</p>
-	if (isLoading) return <p>Loading...</p>
-
-	return <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+	return <MDRenderer url={url} />
 }
 
 //// add robots file
+
+/// choose a font
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,9 @@
 import { Metadata } from "next"
-import Head from "next/head"
 import Link from "next/link"
 
 export default function FourOhFour() {
 	return (
 		<>
-			<Head>
-				<title>wowoworiwoeriu</title>
-			</Head>
 			<h1>404</h1>
 			<p>Page not found</p>
 			<p>
