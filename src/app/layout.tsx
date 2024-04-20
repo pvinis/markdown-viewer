078817d5ea4e13bc36c407efef48747cbbd0cb54
Pavlos Vinieratos <pvinis@gmail.com>
2024-04-20 17:59:16 +0300

    sus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Providers } from "@/Providers"
 import { Metadata } from "next"
 import "./globals.css"
+import { Suspense } from "react"
 
 export const metadata: Metadata = {
 	title: "MD Viewer",
@@ -15,11 +16,13 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>
-				<Providers>
-					<div className="prose prose-quoteless mx-auto max-w-prose px-6 pb-16 pt-32 lg:prose-xl dark:prose-invert prose-blockquote:font-normal prose-blockquote:text-gray-400">
-						{children}
-					</div>
-				</Providers>
+				<Suspense>
+					<Providers>
+						<div className="prose prose-quoteless mx-auto max-w-prose px-6 pb-16 pt-32 lg:prose-xl dark:prose-invert prose-blockquote:font-normal prose-blockquote:text-gray-400">
+							{children}
+						</div>
+					</Providers>
+				</Suspense>
 			</body>
 		</html>
 	)
