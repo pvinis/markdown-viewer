5b53bed8db192846be4463178a58dce112120073
Pavlos Vinieratos <pvinis@gmail.com>
2024-04-22 03:47:47 +0300

    works?

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -3,18 +3,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import posthog from "posthog-js"
 import { PostHogProvider } from "posthog-js/react"
+import { Suspense } from "react"
 
 const queryClient = new QueryClient()
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-	api_host: "/ingest",
-	ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-})
+if (typeof window !== "undefined") {
+	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+		api_host: "/ingest",
+		ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+	})
+}
 
 export function Providers({ children }: React.PropsWithChildren) {
 	return (
-		<PostHogProvider client={posthog}>
-			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-		</PostHogProvider>
+		<Suspense>
+			<PostHogProvider client={posthog}>
+				<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+			</PostHogProvider>
+		</Suspense>
 	)
 }
